516aade82ef261dc44a1d77932d495a9e7d5efb3
sineadkearney <sineadkearney08@gmail.com>
2015-04-28 23:29:44 +0100

    Started on Contacts. Can create contact and add to ordered list

---
+++ C# and Javascript - First Person Game with Phone/Phone/Assets/Scripts/Contact.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Contact : ScriptableObject {
+
+	private string name;
+	private string number;
+
+	public Contact(string newName, string newNumber)
+	{
+		name = newName;
+		number = newNumber;
+	}
+
+	public string GetName()
+	{
+		return name;
+	}
+
+	public void SetName(string newName)
+	{
+		name = newName;
+	}
+
+	public string GetNumber()
+	{
+		return number;
+	}
+
+	public void SetNumber(string newNumber)
+	{
+		number = newNumber;
+	}
+
+}
---
+++ C# and Javascript - First Person Game with Phone/Phone/Assets/Scripts/ContactsCollection.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class ContactsCollection : ScriptableObject {
+
+	private int contactsLength = 0;	
+	public IList<Contact> contacts = new List<Contact>();
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void AddContactToContacts(Contact contact)
+	{
+		if (contactsLength == 0)
+		{
+			contacts.Insert (0, contact);
+		}
+		else
+		{
+			bool added = false;
+			for (int i = 0; i < contactsLength; i ++)
+			{
+				if (string.Compare(contact.GetName(), contacts[i].GetName()) <= 0 && !added)
+				{
+					contacts.Insert (i, contact);
+					added = true;
+				}
+			}
+			if (!added)
+				contacts.Add(contact); //add to end
+		}
+		contactsLength += 1;
+	}
+
+	public void Print()
+	{
+		string str = "";
+		for (int i = 0; i < contactsLength; i++)
+		{
+			str += contacts[i].GetName() + "\n";
+		}
+		Debug.Log (str);
+	}
+	
+}
